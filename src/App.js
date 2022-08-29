8c03112a6201b0093215f7dc5eb9fd163d872f07
Julia Friend <juliafriend@Julias-MacBook-Pro.local>
2022-08-29 14:43:59 -0400

    all routes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ const App = () => {
   const [newDescription, setNewDescription] = useState('');
   const [newImage, setNewImage] = useState('');
   const [newNearby, setNewNearby] = useState('');
+  const [updatedName, setUpdatedName]= useState('')
+  const [updatedLocation, setUpdatedLocation]= useState('')
+  const [updatedDescription, setUpdatedDescription]= useState('')
+  const [updatedImage, setUpdatedImage]= useState('')
+  const [updatedNearby, setUpdatedNearby]= useState('')
 
   const handleNewNameChange = (event) => {
     setNewName(event.target.value)
@@ -26,10 +31,26 @@ const App = () => {
     setNewNearby(event.target.value)
   }
 
+  const updateNewNameChange = (event) => {
+    setUpdatedName(event.target.value)
+  }
+  const updateNewLocationChange = (event) => {
+    setUpdatedLocation(event.target.value)
+  }
+  const updateNewDescriptionChange = (event) => {
+    setUpdatedDescription(event.target.value)
+  }
+  const updateNewImageChange = (event) => {
+    setUpdatedImage(event.target.value)
+  }
+  const updateNewNearbyChange = (event) => {
+    setUpdatedNearby(event.target.value)
+  }
+
   const handleNewTravelSubmit = (event) => {
     event.preventDefault();
     axios.post(
-      'https://arcane-garden-99958.herokuapp.com/',
+      'https://infinite-sands-80753.herokuapp.com/',
       {
         name:newName,
         location:newLocation,
@@ -38,11 +59,105 @@ const App = () => {
         nearby:newNearby
       }
     ).then(()=>{
-      axios.get('https://arcane-garden-99958.herokuapp.com/').then((response) => {
+      axios.get('https://infinite-sands-80753.herokuapp.com/').then((response) => {
         setTravel(response.data)
       })
     })
   }
+  const handleDelete = (travelData) => {
+    axios
+        .delete(`https://infinite-sands-80753.herokuapp.com/${travelData._id}`)
+        .then(() => {
+          axios
+              .get('https://infinite-sands-80753.herokuapp.com/')
+              .then((response) => {
+                setTravel(response.data)
+              });
+        });
+  }
+  const handleUpdateName = (travelData)=>{
+    axios.put(`https://infinite-sands-80753.herokuapp.com/${travelData._id}`,
+        {
+          name: updatedName,
+          location: travelData.location,
+          description: travelData.description,
+          image: travelData.image,
+          nearby: travelData.nearby
+        }
+      ).then((response) => { axios.get('https://infinite-sands-80753.herokuapp.com/')
+          .then((response) => {
+            setTravel(response.data);
+          })
+    })
+  }
+  const handleUpdateLocation = (travelData)=>{
+    axios.put(`https://infinite-sands-80753.herokuapp.com/${travelData._id}`,
+        {
+          name: travelData.name,
+          location: updatedLocation,
+          description: travelData.description,
+          image: travelData.image,
+          nearby: travelData.nearby
+        }
+      ).then((response) => { axios.get('https://infinite-sands-80753.herokuapp.com/')
+          .then((response) => {
+            setTravel(response.data);
+          })
+    })
+  }
+  const handleUpdateDescription = (travelData)=>{
+    axios.put(`https://infinite-sands-80753.herokuapp.com/${travelData._id}`,
+        {
+          name: travelData.name,
+          location: travelData.location,
+          description: updatedDescription,
+          image: travelData.image,
+          nearby: travelData.nearby
+        }
+      ).then((response) => { axios.get('https://infinite-sands-80753.herokuapp.com/')
+          .then((response) => {
+            setTravel(response.data);
+          })
+    })
+  }
+  const handleUpdateImage = (travelData)=>{
+    axios.put(`https://infinite-sands-80753.herokuapp.com/${travelData._id}`,
+        {
+          name: travelData.name,
+          location: travelData.location,
+          description: travelData.description,
+          image: updatedImage,
+          nearby: travelData.nearby
+        }
+      ).then((response) => { axios.get('https://infinite-sands-80753.herokuapp.com/')
+          .then((response) => {
+            setTravel(response.data);
+          })
+    })
+  }
+  const handleUpdateNearby = (travelData)=>{
+    axios.put(`https://infinite-sands-80753.herokuapp.com/${travelData._id}`,
+        {
+          name: travelData.name,
+          location: travelData.location,
+          description: travelData.description,
+          image: travelData.image,
+          nearby: updatedNearby
+        }
+      ).then((response) => { axios.get('https://infinite-sands-80753.herokuapp.com/')
+          .then((response) => {
+            setTravel(response.data);
+          })
+    })
+  }
+  useEffect(()=>{
+    axios
+        .get('https://infinite-sands-80753.herokuapp.com/')
+        .then((response)=>{
+        	setTravel(response.data)
+        })
+},[])
+
 
   return (
     <div className="container">
@@ -57,9 +172,33 @@ const App = () => {
         <input type="submit" value="Post your suggestion"/>
       </form>
     <hr/>
-
+    <h2>See our Suggestions</h2>
+      {travel.map((travel)=>{
+        return (
+          <div>
+            <h2>{travel.name}</h2>
+            <h2>{travel.location}</h2>
+            <p>{travel.description}</p>
+            <img src = {travel.image}/>
+            <p>{travel.nearby}</p>
+            <button onClick={ (event)=>{ handleDelete(travel) } }>Delete</button>
+              <div className='subContainer'>
+              <button className='update' onClick={ (event) => { handleUpdateName(travel) } }>Update Name</button>
+              <input className='update1' type="text" placeholder={travel.name} onKeyUp= {updateNewNameChange}/> <br/>
+              <button className='update' onClick={ (event) => { handleUpdateLocation(travel) } }>Edit Location</button>
+              <input className='update1' type="text" placeholder={travel.location} onKeyUp= {updateNewLocationChange}/> <br/>
+              <button className='update' onClick={ (event) => { handleUpdateDescription(travel) } }>Update Description</button>
+              <input className='update1' type="text" placeholder={travel.description} onKeyUp= {updateNewDescriptionChange}/> <br/>
+              <button className='update' onClick={ (event) => { handleUpdateImage(travel) } }>Update Image</button>
+              <input className='update1' type="text" placeholder={travel.image} onKeyUp= {updateNewImageChange}/> <br/>
+              <button className='update' onClick={ (event) => { handleUpdateNearby(travel) } }>Update Nearby</button>
+              <input className='update1' type="text" placeholder={travel.nearby} onKeyUp= {updateNewNearbyChange}/> <br/>
+            </div> 
+                </div>
+                )
+            })}
     </div>
-  );
+  )
 }
-
+   
 export default App;
