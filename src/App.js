5d6ff01df5bdce0d6d14dab2788ab68a95905a6e
Julia Friend <juliafriend@Julias-MacBook-Pro.local>
2022-08-30 14:42:19 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -159,28 +159,21 @@ const App = () => {
 },[])
 
   return (
-    <div className="container">
-    <h1>Welcome to our Travel Suggestions Page</h1>
-    <h2>Create new suggestion</h2>
-      <form onSubmit={handleNewTravelSubmit}>
-        Name: <input type="text" onChange={handleNewNameChange}/><br/>
-        Location: <input type="text" onChange={handleNewLocationChange}/><br/>
-        Description: <input type="text" onChange={handleNewDescriptionChange}/><br/>
-        Image: <input type="text" onChange={handleNewImageChange}/><br/>
-        Nearby: <input type="text" onChange={handleNewNearbyChange}/><br/>
-        <input type="submit" value="Post your suggestion"/>
-      </form>
-    <hr/>
+    <>
+    <h1 className='header'>Welcome to our Travel Suggestions Page</h1>
+    <div className="mainContainer">
     <h2>See our Suggestions</h2>
       {travel.map((travel)=>{
         return (
-          <div>
+          <div className='container'>
             <h2>{travel.name}</h2>
             <h2>{travel.location}</h2>
             <p>{travel.description}</p>
-            <img src = {travel.image}/>
+            <img className='pic' src = {travel.image}/>
             <p>{travel.nearby}</p>
-            <button onClick={ (event)=>{ handleDelete(travel) } }>Delete</button>
+            <details>
+              <summary>Update Info</summary>
+            <button className='update2' onClick={ (event)=>{ handleDelete(travel) } }>Delete</button>
               <div className='subContainer'>
               <button className='update' onClick={ (event) => { handleUpdateName(travel) } }>Update Name</button>
               <input className='update1' type="text" placeholder={travel.name} onKeyUp= {updateNewNameChange}/> <br/>
@@ -193,10 +186,24 @@ const App = () => {
               <button className='update' onClick={ (event) => { handleUpdateNearby(travel) } }>Update Nearby</button>
               <input className='update1' type="text" placeholder={travel.nearby} onKeyUp= {updateNewNearbyChange}/> <br/>
             </div> 
+            </details>
                 </div>
                 )
             })}
     </div>
+    <br></br>
+    <div className='container'>
+    <h2>Create new suggestion</h2>
+      <form onSubmit={handleNewTravelSubmit}>
+        Name: <input type="text" onChange={handleNewNameChange}/><br/>
+        Location: <input type="text" onChange={handleNewLocationChange}/><br/>
+        Description: <input type="text" onChange={handleNewDescriptionChange}/><br/>
+        Image: <input type="text" onChange={handleNewImageChange}/><br/>
+        Nearby: <input type="text" onChange={handleNewNearbyChange}/><br/>
+        <input type="submit" value="Post your suggestion"/>
+      </form>
+    </div>
+    </>
   )
 }
    
